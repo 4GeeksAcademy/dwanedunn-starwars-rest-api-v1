8c2a46fe8d8ecd20813db34fd39a0101866c6370
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-11 01:01:33 +0000

    build models

--- src/models.py
+++ src/models.py
@@ -4,12 +4,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    username: Mapped[str] = mapped_column(String(50), nullable=False)
 
     def serialize(self):
         return {
@@ -17,3 +19,19 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=True)
+    eye_color: Mapped[str] = mapped_column(Sting(50), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(50), nullable=False)
+    mass: Mapped[float] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(50), nullable=False)
+    gender: Mapped[str] = mapped_column(String(50), nullable=True)
+
+
+class FavoriteUserCharacter(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username = relationship("User", back_populates="FavoriteUserCharacter")
+    email =
